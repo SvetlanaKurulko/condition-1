90e3c3a9a050ba50704b394dd78a81d2cb958715
ПК <svetochka116236@gmail.com>
2023-12-14 17:49:28 +0200

    added conditions theory

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -2,20 +2,42 @@
 # - вивести найменше із трьох чисел
 # - количество однакових чисел
 
-# n1 = int(input("Enter first number: "))
-# n2 = int(input("Enter second number: "))
-# n3 = int(input("Enter third number: "))
-# #
-# # # вывести наименьшее из трех чисел
-# # if n1 < n2 < n3:
-# #     print(n1)
-# # elif n2 < n3 < n1:
-# #     print(n2)
-# # elif n3 < n2 < n1:
-# #     print(n3)
-# # else:
-# #     print("All numbers equals")
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+n3 = int(input("Enter third number: "))
+
+# вывести наименьшее из трех чисел
+# if n1 < n2 < n3:
+#     print(n1)
+# elif n2 < n3 < n1:
+#     print(n2)
+# elif n3 < n2 < n1:
+#     print(n3)
+# else:
+#     print("All numbers equals")
+#
+#
+# # вивести найбільше из трех чисел
+#
+# if n1 > n2 > n3:
+#     print(n1)
+# elif n2 > n3 > n1:
+#     print(n2)
+# elif n3 > n2 > n1:
+#     print(n3)
+# else:
+#     print("All numbers equals")
+
+# вивести середньоарифметичне из трех чисел
+
+# if n1 < n2 > n3:
+#     print(n1)
+# elif n2 < n3 > n1:
+#     print(n2)
+# elif n3 < n2 > n1:
+#     print(n3)
+# else:
+#     print("All numbers equals")
 
 
 
-####
