afc667a43160b68a71301ea53d46167fd3809474
ПК <svetochka116236@gmail.com>
2023-12-14 15:50:58 +0200

    added homework tasks

---
+++ pythonProject3/main.py
@@ -0,0 +1,17 @@
+# ввести з клавіатури 3 числа
+# - вивести найменше із трьох чисел
+# - количество однакових чисел
+
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+# #
+# # # вывести наименьшее из трех чисел
+# # if n1 < n2 < n3:
+# #     print(n1)
+# # elif n2 < n3 < n1:
+# #     print(n2)
+# # elif n3 < n2 < n1:
+# #     print(n3)
+# # else:
+# #     print("All numbers equals")
