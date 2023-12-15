d26f5dd281c9571b8f6573767e254bff49e9e847
ПК <svetochka116236@gmail.com>
2023-12-15 09:44:13 +0200

    implemented first task

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -1,43 +1,24 @@
-# ввести з клавіатури 3 числа
-# - вивести найменше із трьох чисел
-# - количество однакових чисел
+# Користувач вводить три цифри з клавіатури.
+# Залежно від вибору користувача програма виводить на екран максимум із трьох,
+# мінімум із трьох або середньоарифметичне трьох чисел.
 
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
 n3 = int(input("Enter third number: "))
 
-# вывести наименьшее из трех чисел
-# if n1 < n2 < n3:
-#     print(n1)
-# elif n2 < n3 < n1:
-#     print(n2)
-# elif n3 < n2 < n1:
-#     print(n3)
-# else:
-#     print("All numbers equals")
-#
-#
-# # вивести найбільше из трех чисел
-#
-# if n1 > n2 > n3:
-#     print(n1)
-# elif n2 > n3 > n1:
-#     print(n2)
-# elif n3 > n2 > n1:
-#     print(n3)
-# else:
-#     print("All numbers equals")
+choice = int(input("Enter your choice (1/2/3): "))
 
-# вивести середньоарифметичне из трех чисел
-
-# if n1 < n2 > n3:
-#     print(n1)
-# elif n2 < n3 > n1:
-#     print(n2)
-# elif n3 < n2 > n1:
-#     print(n3)
-# else:
-#     print("All numbers equals")
+if choice == 1:
+    maximum = max(n1, n2, n3)
+    print("Maximum of the three numbers is:", maximum)
+elif choice == 2:
+    minimum = min(n1, n2, n3)
+    print("Minimum of the three numbers is:", minimum)
+elif choice == 3:
+    average = (n1 + n2 + n3) / 3
+    print("Average of the three numbers is:", average)
+else:
+    print("Invalid choice")
 
 
 
